c74c515fd072bc431717381ea56b7694676308ec
Emanuele <em.zaccaria@gmail.com>
2020-04-21 16:07:50 +0200

    Starting Point

---
+++ myscript.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  // // gestione Handlebars:
+  // // Prendo quello che è contenuto nello script selezionandolo tramite id
+  // var source = $("#template-mex-inviato").html();
+  // // ciò che ho preso lo do a Handlebars e glielo faccio smaneggiare
+  // var template = Handlebars.compile(source);
+  // // creo una variabile che contiene l'informazione completa del tamplate + il testo inserito dinamicamente
+  // var html = template(testoInputObject);
+  // // inserisco l'informazione del template "html" dove voglio io
+  // $(".DoveVoglioIo").append(html);
+
+
+});
