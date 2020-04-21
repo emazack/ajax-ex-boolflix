8a5664bf8c653f500a05463a62c323cd57be808d
Emanuele <em.zaccaria@gmail.com>
2020-04-21 17:50:54 +0200

    JS getting data al API example

--- myscript.js
+++ myscript.js
@@ -1,13 +1,26 @@
+// per provare
+// https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+futuro
+
+// la mia
+// "https://api.themoviedb.org/3/movie/550?api_key=e378742730fc784d9e5410ffaa7cd73e"
+
+
 
 $(document).ready(function(){
 
   $("#bottone-ricerca").click(
     function(){
       $.ajax({
-        url : "https://api.themoviedb.org/3/movie/550?api_key=e378742730fc784d9e5410ffaa7cd73e",
+        url : "https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+futuro",
         method : "GET",
         success: function (data,stato) {
-
+          var risultati = data.results;
+          for (var i = 0; i < risultati.length; i++) {
+            console.log(risultati[i]["title"]);
+            console.log(risultati[i]["original_title"]);
+            console.log(risultati[i]["original_language"]);
+            console.log(risultati[i]["vote_average"]);
+          }
         },
         error : function (richiesta, stato, errore) {
           // quello che succede se c'è un errore. Ex:
